a3d89d0d2eb12da718c4fd3f88bc26bd5e95925d
Sébastien Dan <sebastien.dan@gmail.com>
2023-09-18 12:59:24 +0200

    fix: use request headers for controller throlling

--- packages/backend/src/faucet/faucet.controller.ts
+++ packages/backend/src/faucet/faucet.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common'
+import { Body, Controller, Headers, Post, UseGuards } from '@nestjs/common'
 
 import { GetSubnetAssetsDto } from './faucet.dto'
 import { FaucetService } from './faucet.service'
+import { ThrottlerBehindProxyGuard } from './throttler.guard'
 
 @Controller('faucet')
 export class FaucetController {
   constructor(private faucetService: FaucetService) {}
 
   @Post('getSubnetAssets')
+  @UseGuards(ThrottlerBehindProxyGuard)
   async getSubnetAssets(
     @Body() getSubnetAssetsDto: GetSubnetAssetsDto,
     @Headers('traceparent') traceparent: string
