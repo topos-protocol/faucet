3e16e52ffe50a113a3f90c17b0ba3734829d5ab6
Sébastien Dan <sebastien.dan@gmail.com>
2024-01-09 11:24:50 +0100

    feat: add version to footer

--- packages/frontend/src/components/Footer.tsx
+++ packages/frontend/src/components/Footer.tsx
@@ -1,9 +1,18 @@
-import { Layout } from 'antd'
+import { Col, Layout, Row } from 'antd'
+
+import packageJson from '../../../../package.json'
 
 const { Footer: AntdFooter } = Layout
 
-const Footer = () => (
-  <AntdFooter style={{ textAlign: 'center' }}>zk Foundation ©2023</AntdFooter>
-)
+const Footer = () => {
+  return (
+    <AntdFooter>
+      <Row justify="center" gutter={8}>
+        <Col>zk Foundation © {new Date().getFullYear()}</Col>
+        <Col>v{packageJson.version}</Col>
+      </Row>
+    </AntdFooter>
+  )
+}
 
 export default Footer
