11fa8859870e859b6c7c0b1355f0368537f02067
Sébastien Dan <sebastien.dan@gmail.com>
2024-02-02 18:17:50 +0100

    feat: add e2e test

--- packages/frontend/cypress/e2e/home.cy.ts
+++ packages/frontend/cypress/e2e/home.cy.ts
@@ -1,30 +1,11 @@
-import { INFO } from '../../src/constants/wordings'
-
 describe('Home', () => {
   beforeEach(() => {
     cy.visit('/')
   })
 
-  it('should have Builders Program banner', () => {
-    cy.get('.ant-alert-banner')
-      .as('banner')
-      .find('span')
-      .contains(INFO.JOIN_BUILDERS_PROGRAM)
-      .should('exist')
-      .and('be.visible')
-    cy.get('@banner')
-      .find('a')
-      .as('banner-link')
-      .should('have.text', INFO.BUILDERS_PROGRAM)
-      .and(
-        'have.attr',
-        'href',
-        'https://builders.toposware.com/topos-builders-program-v1-0'
-      )
-      .and('not.be.disabled')
+  it('should have visible and enabled address input', () => {
+    cy.get('#faucet_address').should('be.visible').and('be.enabled')
   })
 
-  it('should have disabled address input', () => {
-    cy.get('#faucet_address').should('be.visible').and('be.disabled')
-  })
+  // 0x4aab25b4fad0beaac466050f3a7142a502f4cf0a
 })
--- packages/frontend/src/constants/wordings.ts
+++ packages/frontend/src/constants/wordings.ts
@@ -1,8 +1,3 @@
-export enum INFO {
-  BUILDERS_PROGRAM = 'Builders Program',
-  JOIN_BUILDERS_PROGRAM = 'Building with Topos? Apply for a grant!',
-}
-
 export enum SUCCESS {
   GET_SUBNET_ASSET = 'Coins were successfully sent to your address on the selected subnets 🎉',
 }
